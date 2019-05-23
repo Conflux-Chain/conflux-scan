c93631792ecf70ef6c8a9604f52d542212f12652
Angelia <greenfish84@126.com>
2019-05-23 18:43:33 +0800

    modify page

--- src/pages/Epoch/list.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-function List() {
-  return <div className="page-epoch-list" />;
-}
-export default List;
---
+++ src/pages/NotFoundTx/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+function NotFoundTx() {
+  return <div className="page-notfound-tx" />;
+}
+export default NotFoundTx;
