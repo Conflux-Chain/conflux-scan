c331950f7143edbd37e211c0fd8f46782c511f62
conflux-Schwarz <alichs@SchwarzdeMacBook-Pro.local>
2019-05-24 17:28:54 +0800

    fix

--- src/components/DataList/index.js
+++ src/components/DataList/index.js
@@ -1,8 +1,67 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import '../../assets/semantic-ui/semantic.css';
 
-function DataList() {
-  return <div />;
+function DataList({
+  showHeader = false,
+  columns = [
+    {
+      key: 1,
+      dataIndex: 'ein',
+      title: 'Blocks',
+      render: (text, row) => (
+        <div>
+          <img href="https://place-hold.it/40x40" width="40" height="40" />
+        </div>
+      ),
+    },
+    {
+      key: 2,
+      dataIndex: 'zwei',
+      title: 'Blocks',
+    },
+  ],
+  dataSource = [{ key: 1, ein: '80580', zwei: '0x123' }, { key: 2, ein: '80581', zwei: '0x124' }],
+}) {
+  return (
+    <div>
+      <table className="ui very basic table">
+        {!showHeader ? null : (
+          <thead>
+            <tr>
+              {columns.map((item) => (
+                <th colSpan={item.colSpan ? item.colSpan : 1} key={item.key}>
+                  {item.title}
+                </th>
+              ))}
+            </tr>
+          </thead>
+        )}
+        <tbody>
+          {dataSource.map((item) => (
+            <tr key={item.key}>
+              {columns.map((unit) => (
+                <th className={unit.className ? unit.className : ''} key={unit.key}>
+                  {!unit.render ? item[unit.dataIndex] : unit.render(item[unit.dataIndex], item)}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
+DataList.propTypes = {
+  showHeader: PropTypes.bool,
+  columns: PropTypes.arrayOf(PropTypes.object),
+  dataSource: PropTypes.arrayOf(PropTypes.object),
+};
+DataList.defaultProps = {
+  showHeader: false,
+  columns: [],
+  dataSource: [],
+};
 
 export default DataList;
--- src/pages/BlockAndTxn/index.js
+++ src/pages/BlockAndTxn/index.js
@@ -1,6 +1,92 @@
 import React from 'react';
+import { Card, Feed } from 'semantic-ui-react';
+import styled from 'styled-components';
+import DataList from '../../components/DataList';
+
+const Wrapper = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+`;
+
+const StyledTabel = styled.div`
+  margin-top: 20px;
+  width: calc(50% - 6px);
+
+  &.left {
+    float: left;
+  }
+  &.right {
+    float: right;
+  }
+`;
+
+const CardExampleContentBlock = () => (
+  <Card>
+    <Card.Content>
+      <Card.Header>Recent Activity</Card.Header>
+    </Card.Content>
+    <Card.Content>
+      <Feed>
+        <Feed.Event>
+          <Feed.Label image="/images/avatar/small/jenny.jpg" />
+          <Feed.Content>
+            <Feed.Date content="1 day ago" />
+            <Feed.Summary>
+              You added <a>Jenny Hess</a> to your <a>coworker</a> group.
+            </Feed.Summary>
+          </Feed.Content>
+        </Feed.Event>
+
+        <Feed.Event>
+          <Feed.Label image="/images/avatar/small/molly.png" />
+          <Feed.Content>
+            <Feed.Date content="3 days ago" />
+            <Feed.Summary>
+              You added <a>Molly Malone</a> as a friend.
+            </Feed.Summary>
+          </Feed.Content>
+        </Feed.Event>
+
+        <Feed.Event>
+          <Feed.Label image="/images/avatar/small/elliot.jpg" />
+          <Feed.Content>
+            <Feed.Date content="4 days ago" />
+            <Feed.Summary>
+              You added <a>Elliot Baker</a> to your <a>musicians</a> group.
+            </Feed.Summary>
+          </Feed.Content>
+        </Feed.Event>
+      </Feed>
+    </Card.Content>
+  </Card>
+);
 
 function BlockAndTxn() {
-  return <div className="page-block-txn" />;
+  return <CardExampleContentBlock />;
+  // <div className="page-block-txn" >
+  //   <CardExampleContentBlock />
+  //   <Wrapper>
+  //     <StyledTabel className='left'>
+  //       <Card>
+  //         <Card.Content>
+  //           <Card.Header>Recent Activity</Card.Header>
+  //         </Card.Content>
+  //         <Card.Content>
+  //           <DataList />
+  //         </Card.Content>
+  //       </Card>
+  //     </StyledTabel>
+  //     <StyledTabel className='right'>
+  //       <Card>
+  //         <Card.Content>
+  //           <Card.Header>Recent Activity</Card.Header>
+  //         </Card.Content>
+  //         <Card.Content>
+  //           <DataList />
+  //         </Card.Content>
+  //       </Card>
+  //     </StyledTabel>
+  //   </Wrapper>
+  // </div>;
 }
-export default BlockAndTxn;
+export default CardExampleContentBlock;
