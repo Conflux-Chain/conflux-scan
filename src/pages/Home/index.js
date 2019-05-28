09bc229a601edf81605f7dad7f8ec1207c1dbe12
Angelia <greenfish84@126.com>
2019-05-28 11:02:57 +0800

    dashboard

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
-
-const More = require('../../assets/images/icons/more.svg');
+import moreBlue from '../../assets/images/icons/more_blue.svg';
+import moreRed from '../../assets/images/icons/more_red.svg';
 
 const Container = styled.div`
   width: 100%;
@@ -34,8 +34,18 @@ const Block = styled.div`
     margin-top: 8px;
     .block-value {
       font-size: 24px;
-      line-height: 30px;
+      line-height: 28px;
       font-weight: 700;
+    }
+    .block-unit {
+      font-size: 14px;
+      line-height: 18px;
+      margin-left: 8px;
+      color: rgba(0, 0, 0, 0.87);
+    }
+    .block-rate {
+      display: flex;
+      align-items: baseline;
     }
     .block-diff-up {
       font-size: 16px;
@@ -45,21 +55,25 @@ const Block = styled.div`
       font-size: 16px;
       color: #b00020;
     }
-    .icon-arrow-top {
+    .icon-arrow-up {
       display: inline-block;
+      background-image: url(${moreBlue});
+      background-size: 11px auto;
+      background-repeat: no-repeat;
       transform: scaleX(-1) rotate(-90deg);
-      color: #311b92;
       width: 11px;
       height: 12px;
       margin-left: 6px;
     }
     .icon-arrow-down {
       display: inline-block;
+      background-image: url(${moreRed});
+      background-size: 11px auto;
+      background-repeat: no-repeat;
       transform: scaleX(-1) rotate(90deg);
       width: 11px;
       height: 12px;
       margin-left: 6px;
-      color: #b00020 !important;
     }
   }
 `;
@@ -74,7 +88,7 @@ function Home() {
             <span className="block-value">774.26</span>
             <span className="block-diff-up">
               + 4.2 %
-              <img className="icon-arrow-top" src={More} />
+              <span className="icon-arrow-up" />
             </span>
           </div>
         </Block>
@@ -84,7 +98,7 @@ function Home() {
             <span className="block-value">200,570,130</span>
             <span className="block-diff-down">
               - 13.88 %
-              <img className="icon-arrow-down" src={More} />
+              <span className="icon-arrow-down" />
             </span>
           </div>
         </Block>
@@ -94,21 +108,20 @@ function Home() {
             <span className="block-value">4.67</span>
             <span className="block-diff-up">
               + 0 %
-              <svg className="icon-arrow-down" aria-hidden="true">
-                <use xlinkHref="#iconmore" />
-              </svg>
+              <span className="icon-arrow-up" />
             </span>
           </div>
         </Block>
         <Block>
           <span className="block-title">Hash Rate</span>
           <div className="block-content">
-            <span className="block-value">42.95</span>
+            <span className="block-rate">
+              <span className="block-value">42.95</span>
+              <span className="block-unit">MH/s</span>
+            </span>
             <span className="block-diff-up">
-              + 0 %
-              <svg className="icon-arrow-down" aria-hidden="true">
-                <use xlinkHref="#iconmore" />
-              </svg>
+              + 16.11 %
+              <span className="icon-arrow-up" />
             </span>
           </div>
         </Block>
