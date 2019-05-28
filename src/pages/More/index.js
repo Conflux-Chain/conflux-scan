09e274db7143dd8a53d220f3d459c3794576b430
wenzhe <wenzhe@gilberts-MacBook-Pro.local>
2019-05-28 15:45:13 +0800

    add nodeMap comp

--- src/pages/More/index.js
+++ src/pages/More/index.js
@@ -1,6 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
+import NodeMap from '../../components/NodeMap';
+import exampleData from '../../components/NodeMap/example';
 
-function More() {
-  return <div className="page-more" />;
+class More extends Component {
+  constructor(...args) {
+    super(...args);
+    this.state = {
+      scatterData: [],
+    };
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        scatterData: exampleData,
+      });
+      console.log('sync');
+    }, 2000);
+  }
+
+  render() {
+    const { scatterData } = this.state;
+    return (
+      <div id="map-div">
+        <NodeMap
+          scatterData={scatterData}
+          style={{
+            height: 800,
+            width: 1000,
+          }}
+        />
+      </div>
+    );
+  }
 }
 export default More;
