dfa379a647e1d226cbe8e94ec246c3ae62f8d1d9
wenzhe <wenzhe@conflux-chain.org>
2019-06-03 18:05:03 +0800

    webpackcdn add js combine&polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,93 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const WebpackCdnPlugin = require('webpack-cdn-plugin');
+const fs = require('fs');
 
 const devMode = process.env.NODE_ENV !== 'production';
+/* eslint no-underscore-dangle: 0 */
+const cdnGetJs = WebpackCdnPlugin._getJs;
+const cdnModules = [
+  {
+    name: 'react',
+    var: 'React',
+    path: 'umd/react.production.min.js',
+  },
+  {
+    name: 'prop-types',
+    var: 'PropTypes',
+    path: 'prop-types.min.js',
+  },
+  {
+    name: 'react-dom',
+    var: 'ReactDOM',
+    path: 'umd/react-dom.production.min.js',
+  },
+  {
+    name: 'react-router-dom',
+    var: 'ReactRouterDOM',
+    path: 'umd/react-router-dom.min.js',
+  },
+  {
+    name: 'react-intl',
+    var: 'ReactIntl',
+    path: 'dist/react-intl.min.js',
+  },
+  {
+    name: 'moment',
+    var: 'moment',
+    path: 'min/moment-with-locales.js',
+  },
+  {
+    name: 'styled-components',
+    var: 'styled',
+    path: 'dist/styled-components.min.js',
+  },
+  {
+    name: 'echarts',
+    var: 'echarts',
+    path: 'dist/echarts.min.js',
+  },
+];
+function cpFile(inFilePath, outFilePath) {
+  return new Promise((resolve, reject) => {
+    const writeFile = fs.createWriteStream(outFilePath, {
+      flags: 'a',
+    });
+    fs.createReadStream(inFilePath).pipe(writeFile);
+    writeFile.on('error', reject);
+    writeFile.on('close', resolve);
+  });
+}
+
+const vendorPlugin = {
+  apply: (compiler) => {
+    compiler.hooks.done.tap('vendorPlugin', () => {
+      if (devMode) return;
+      const writeVendor = async () => {
+        for (let i = 0; i < cdnModules.length; i++) {
+          /* eslint no-await-in-loop: 0 */
+          await cpFile(
+            path.resolve(__dirname, './node_modules', cdnModules[i].name, cdnModules[i].path),
+            path.resolve(__dirname, './dist/vendor.js')
+          );
+        }
+      };
+      writeVendor();
+    });
+  },
+};
+
+WebpackCdnPlugin._getJs = (...args) => {
+  const cdnUrls = cdnGetJs(...args);
+  if (devMode === false) {
+    const combineUrl = 'https://cdn.jsdelivr.net/combine/';
+    const cdnUrlPath = cdnUrls.map((v) => {
+      return v.replace('https://cdn.jsdelivr.net/', '');
+    });
+    return [combineUrl + cdnUrlPath.join(',')];
+  }
+  return cdnUrls;
+};
 
 module.exports = {
   entry: path.resolve(__dirname, 'src', 'index.js'), // 项目的起点入口
@@ -30,51 +115,11 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'src', 'index.html'), // 模板
       filename: 'index.html',
+      inject: false,
     }), // 生成Html5文件
     new WebpackCdnPlugin({
-      modules: [
-        {
-          name: 'react',
-          var: 'React',
-          path: 'umd/react.production.min.js',
-        },
-        {
-          name: 'prop-types',
-          var: 'PropTypes',
-          path: 'prop-types.min.js',
-        },
-        {
-          name: 'react-dom',
-          var: 'ReactDOM',
-          path: 'umd/react-dom.production.min.js',
-        },
-        {
-          name: 'react-router-dom',
-          var: 'ReactRouterDOM',
-          path: 'umd/react-router-dom.min.js',
-        },
-        {
-          name: 'react-intl',
-          var: 'ReactIntl',
-          path: 'dist/react-intl.min.js',
-        },
-        {
-          name: 'moment',
-          var: 'moment',
-          path: 'min/moment-with-locales.js',
-        },
-        {
-          name: 'styled-components',
-          var: 'styled',
-          path: 'dist/styled-components.min.js',
-        },
-        {
-          name: 'echarts',
-          var: 'echarts',
-          path: 'dist/echarts.min.js',
-        },
-      ],
-      prod: process.env.NODE_ENV === 'production',
+      modules: cdnModules.map((v) => Object.assign({}, v)),
+      prod: !devMode,
       publicPath: '/node_modules',
       prodUrl: 'https://cdn.jsdelivr.net/npm/:name@:version/:path',
     }),
@@ -82,6 +127,7 @@ module.exports = {
       filename: devMode ? '[name].css' : '[name].[hash].css',
       chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
     }),
+    vendorPlugin,
   ],
   module: {
     // 模块加载
