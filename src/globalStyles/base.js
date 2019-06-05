d7f638122a6d63ec3ade72bd5fecbb8079a62c35
Angelia <greenfish84@126.com>
2019-06-05 21:42:29 +0800

    font-size

--- src/globalStyles/base.js
+++ src/globalStyles/base.js
@@ -59,7 +59,7 @@ const BaseStyle = css`
 
   body {
     font-family: 'Proxima Nova', 'Helvetica Neue', Helvetica, Arial, sans-serif;
-    font-size: 14px;
+    font-size: 16px;
     line-height: 1.6;
     font-weight: 400;
     word-wrap: break-word;
