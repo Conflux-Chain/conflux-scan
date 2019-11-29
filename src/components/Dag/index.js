18a598aea350351e3582fc65adacb6374f55c76a
yqrashawn <namy.19@gmail.com>
2019-11-29 16:06:13 +0800

    Add: add interactions to dag

--- src/components/Dag/index.js
+++ src/components/Dag/index.js
@@ -1,4 +1,4 @@
-import React, { memo } from 'react';
+import React, { memo, useState } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { useMount } from 'react-use';
@@ -34,8 +34,46 @@ function startFechingDagData() {
 }
 
 const Container = styled.div``;
+const Tooltip = styled.div`
+  padding: 14px 13px;
+  border-radius: 4px;
+  box-shadow: 1px 2px 8px rgba(0, 0, 0, 0.16);
+  background: white;
+  position: absolute;
+  color: #5c5c5c;
+  font-size: 12px;
+  line-height: 12px;
+  white-space: pre-wrap;
+  pointer-event: none;
+`;
+const ArrowLeft = styled.div`
+  position: absolute;
+  left: -6px;
+  width: 0;
+  height: 0;
+  border-top: 6.5px solid transparent;
+  border-bottom: 6.5px solid transparent;
+  border-right: 6.5px solid white;
+`;
+const ArrowRight = styled.div`
+  position: absolute;
+  right: -6px;
+  width: 0;
+  height: 0;
+  border-top: 6.5px solid transparent;
+  border-bottom: 6.5px solid transparent;
+  border-left: 6.5px solid white;
+`;
+
+const pointSize = 20;
 
 function Dag({ id = 'dag-viewer' } = {}) {
+  const [tooltipOpt, setTooltipOpt] = useState({
+    transform: '',
+    direction: '',
+    text: '',
+    style: { top: 0, left: 0, visibility: 'hidden' },
+  });
   useMount(async () => {
     await loadDagJs();
     const Player = window.ConfluxDagPlayer;
@@ -45,18 +83,36 @@ function Dag({ id = 'dag-viewer' } = {}) {
       backgroundColor: '0x0B3560',
       doc: container,
       playByDefault: 500,
-      pointSize: 20,
+      pointSize,
       globalRadius: 100,
       defaultInterval: 500,
       colors: [0x76e2e0, 0xc79af5, 0xe4dcef, 0xf2a9b7, 0xf2be81, 0xc29af5, 0xdfdcef, 0xeca987, 0xe087ad, 0x85cfe8, 0xe0e0e0],
       chain: initialSubChains.pop().reverse(),
       onBlockClick: ({ hash }) => {
-        window.open(`https://confluxscan.io/blocksdetail/${hash}`);
+        window.open(`/blocksdetail/${hash}`);
       },
-      onBlockMouseOver: (_, p) => {
+      onBlockMouseOver: ({ mesh, meshPosition }, p) => {
+        if (mesh.refBlocksInfo.length) {
+          let refHashes = '';
+          refHashes = 'Ref block hashes:\n';
+          // eslint-disable-next-line no-return-assign
+          mesh.refBlocksInfo.forEach(({ hash: refHash }, idx) => (refHashes += `[${idx}]:${refHash}\n`));
+          const useRightArrow = meshPosition.x > document.getElementById(id).clientWidth / 2;
+          setTooltipOpt({
+            direction: useRightArrow ? 'right' : 'left',
+            text: refHashes,
+            style: {
+              transform: useRightArrow ? 'translateX(-100%)' : undefined,
+              left: `${useRightArrow ? meshPosition.x - pointSize * 2 : meshPosition.x + pointSize}px`,
+              top: `${meshPosition.y - pointSize}px`,
+              visibility: 'visible',
+            },
+          });
+        }
         p.pause();
       },
       onBlockMouseOut: (_, p) => {
+        setTooltipOpt({ style: { visibility: 'hidden' } });
         p.play();
       },
       // debug: true,
@@ -64,8 +120,17 @@ function Dag({ id = 'dag-viewer' } = {}) {
     appendAllSubChain(player, initialSubChains);
     startFechingDagData.call(player);
   });
+  const { style, direction, text } = tooltipOpt;
 
-  return <Container id={id} />;
+  return (
+    <Container id={id}>
+      <Tooltip style={style}>
+        {direction === 'left' && <ArrowLeft />}
+        {direction === 'right' && <ArrowRight />}
+        {text}
+      </Tooltip>
+    </Container>
+  );
 }
 
 Dag.defaultProps = { id: 'dag-viewer' };
