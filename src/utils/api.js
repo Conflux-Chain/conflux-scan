d8dd4e67e9dd5bcdd896e1442e51c324cab3608a
21paradox <1036339815@qq.com>
2020-05-25 12:58:33 +0800

    code remove

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,31 +3,6 @@ import { futurePrefix, contractMangerPrefix, UPDATE_CONTRACT_MANAGER_CACHE, erro
 import { cfx, cfxUtil } from './transaction';
 import { toast } from '../components/Toast';
 
-export const reqFcStat = (param) => {
-  return sendRequest({
-    // url: 'http://yapi.conflux-chain.org/mock/4/dashboard/fc/statistics',
-    url: '/api/dashboard/fc/statistics',
-    query: param,
-  }).then((res) => res.body);
-};
-
-export const reqFcList = (param, options = {}) => {
-  return sendRequest({
-    // url: 'http://yapi.conflux-chain.org/mock/4/transaction/fc/list',
-    url: '/api/transaction/fc/list',
-    query: param,
-    showError: options.showError,
-  }).then((res) => res.body);
-};
-
-export const reqFcByAddress = (param) => {
-  return sendRequest({
-    url: `/api/account/${param.address}/fc`,
-    // url: 'http://yapi.conflux-chain.org/mock/4/account/:address/fc',
-    query: param,
-  }).then((res) => res.body);
-};
-
 export const reqRecentDagBlock = () => {
   return sendRequest({ url: `${futurePrefix}/dashboard/dag` }).then((res) => res.body);
 };
